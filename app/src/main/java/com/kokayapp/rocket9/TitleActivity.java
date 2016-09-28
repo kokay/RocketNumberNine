5f62306c03e838e2f84c87b9d159c9fd91aa3e57
Koji <a.cz0512@gmail.com>
2016-09-28 01:30:24 -0500

    Initial commit

---
+++ app/src/main/java/com/kokayapp/rocket9/TitleActivity.java
@@ -0,0 +1,13 @@
+package com.kokayapp.rocket9;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TitleActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title);
+    }
+}
