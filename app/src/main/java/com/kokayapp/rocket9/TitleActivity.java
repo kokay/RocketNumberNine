c727299e5c335a4f99821a5aa468b25022893e38
Koji <a.cz0512@gmail.com>
2016-09-28 01:46:44 -0500

    Add title screen

--- app/src/main/java/com/kokayapp/rocket9/TitleActivity.java
+++ app/src/main/java/com/kokayapp/rocket9/TitleActivity.java
@@ -1,7 +1,9 @@
 package com.kokayapp.rocket9;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TitleActivity extends Activity {
 
@@ -9,5 +11,10 @@ public class TitleActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
+    }
+
+    public void clickPlay(View view) {
+        //Intent intent = new Intent(this, GameActivity.class);
+        //startActivity(intent);
     }
 }
