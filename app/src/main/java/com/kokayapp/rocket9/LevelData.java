c720e3e836329b0440681bd0110eba0a92311b3e
Koji <a.cz0512@gmail.com>
2016-09-29 16:04:25 -0500

    Add foreground ListArray and move one background

--- app/src/main/java/com/kokayapp/rocket9/LevelData.java
+++ app/src/main/java/com/kokayapp/rocket9/LevelData.java
@@ -13,6 +13,7 @@ public class LevelData {
     private Rocket rocket;
     private ArrayList<Enemy> enemies;
     private ArrayList<Background> backgrounds;
+    private ArrayList<Background> foregrounds;
 
     public LevelData(Context context, Viewport vp) {
         rocket = new Rocket(context, vp);
@@ -22,19 +23,22 @@ public class LevelData {
         enemies.add(new RedEnemy(context, vp, 50, 5));
         backgrounds = new ArrayList<>();
         backgrounds.add(new Background(context, vp, R.drawable.middleground, 0, 32, 30));
-        backgrounds.add(new Background(context, vp, R.drawable.foreground, 0, 32, 300));
+        foregrounds = new ArrayList<>();
+        foregrounds.add(new Background(context, vp, R.drawable.foreground, 0, 32, 300));
     }
 
-    public void update(long fps) {
-        rocket.update(fps);
-        for(Enemy e : enemies) e.update(fps, rocket);
+    public void update(long fps, Viewport vp) {
+        rocket.update(fps, vp);
+        for(Enemy e : enemies) e.update(fps, vp, rocket);
         for(Background bg : backgrounds) bg.update(fps);
+        for(Background fg : foregrounds) fg.update(fps);
     }
 
     public void draw(Canvas canvas, Viewport vp) {
+        for(Background bg : backgrounds) bg.draw(canvas, vp);
         for(Enemy enemy : enemies) enemy.draw(canvas, vp);
         rocket.draw(canvas, vp);
-        for(Background bg : backgrounds) bg.draw(canvas, vp);
+        for(Background fg : foregrounds) fg.draw(canvas, vp);
 
         for(Enemy enemy : enemies) enemy.drawHealth(canvas, vp);
         rocket.drawHealth(canvas, vp);
